f64ddbd71938b264c19230bb21c27142799970c6
Nikitos <nikita4651@yandex.ru>
2026-06-29 18:29:14 +0300

    Задание 2

--- src/test/java/ru/netology/servic/CardTest.java
+++ src/test/java/ru/netology/servic/CardTest.java
@@ -2,10 +2,11 @@ package ru.netology.servic;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
+
+
 
 import org.junit.jupiter.api.Test;
+
 
 import org.openqa.selenium.Keys;
 
@@ -13,43 +14,48 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.$;
 
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
 
 
 class CardTest {
-    public String generateDate(int days, String pattern){
+    public String generateDate(int days, String pattern) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
     void cardWithDeliveryTest() {
         String planningDate = generateDate(4, "dd.MM.yyyy");
-        Selenide.open("http://localhost:9999");
+        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Кострома");
 
         $("[data-test-id='date'] input")
 
-                //.shouldBe(Condition.visible)
-                //.shouldBe(Condition.enabled).setValue(planningDate)
                 .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE)
                 .setValue(planningDate);
 
         $("[data-test-id='name'] input").setValue("Осепчук Никита");
         $("[data-test-id='phone'] input").setValue("+79110126430");
         $("[data-test-id='agreement']").click();
-        //$("button").click();
-       $(Selectors.withText("Забронировать")).click();
+
+        $(Selectors.withText("Забронировать")).click();
         $("div[data-test-id='notification']")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.text("Встреча успешно забронирована на"));
 
-       // $(Selectors.withText("Встреча успешно забронирована на"))
-        //        .should(Condition.visible, Duration.ofSeconds(15))
-       //         .should(Condition.visible);
-       // String expectedFragment = planningDate;
-       // String actualText = $(Selectors.withText("Встреча успешно забронирована на")).getText();
 
+
+
+    }
+
+    @Test
+    void shouldRegisterCardDelivery() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Ко");
+        $$("div.popup__content div").find(exactText("Кострома")).click();
+        $("[data-test-id='date'] button").click();
 
     }
 }
