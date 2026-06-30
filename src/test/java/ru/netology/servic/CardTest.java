788d0f1b8e5a677f6bd0d2b5e8cf3585077196fa
Nikitos <nikita4651@yandex.ru>
2026-06-30 22:27:01 +0300

    Исправил

--- src/test/java/ru/netology/servic/CardTest.java
+++ src/test/java/ru/netology/servic/CardTest.java
@@ -2,42 +2,20 @@ package ru.netology.servic;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
-
-
-import com.codeborne.selenide.Selenide;
-;
 import org.junit.jupiter.api.Test;
-
-
 import org.openqa.selenium.Keys;
-
-
 import java.time.Duration;
 import java.time.LocalDate;
-
 import java.time.format.DateTimeFormatter;
-
-
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 
 class CardTest {
-    public String generateDate(int days, String pattern) {
+    public String generateDate(int day, String pattern) {
         return LocalDate.now()
-                .plusDays(days)
+                .plusDays(day)
                 .format(DateTimeFormatter.ofPattern(pattern));
 
-
-    }
-
-    public String generateDateMonthYear(int days, int month, int year, String pattern) {
-        return LocalDate.now()
-                .plusDays(days)
-                .plusMonths(month)
-                .plusYears(year)
-                .format(DateTimeFormatter.ofPattern(pattern));
 
     }
 
@@ -48,110 +26,43 @@ class CardTest {
         String planningDate = generateDate(4, "dd.MM.yyyy");
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Кострома");
-
         $("[data-test-id='date'] input")
-
                 .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE)
                 .setValue(planningDate);
-
         $("[data-test-id='name'] input").setValue("Осепчук Никита");
         $("[data-test-id='phone'] input").setValue("+79110126430");
         $("[data-test-id='agreement']").click();
-
         $(Selectors.withText("Забронировать")).click();
-        $("div[data-test-id='notification']")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(text("Встреча успешно забронирована на"));
-
-
-    }
-
-
-    @Test
-    void shouldRegisterCardDelivery() {
-
-
-        LocalDate target = LocalDate.now().plusWeeks(1);
-        String dayText = String.format("%01d", target.getDayOfMonth());
-        int monthsToScroll = 0;
-
-
-        System.out.println("DEBUG: dayText = '" + dayText + "' (length=" + dayText.length() + ")");
-        open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Ко");
-        $$("div.popup__content div").find(exactText("Кострома")).click();
-        // $("[data-test-id='date'] button").click();
-        // Открытие календаря (только один клик по кнопке)
-        $("[data-test-id='date'] button").click();
-
-        for (int i = 0; i < monthsToScroll; i++) {
-            var arrows = $$("div.calendar__arrow.calendar__arrow_direction_right");
-            if (arrows.isEmpty()) {
-                throw new IllegalStateException("Не найдены стрелки календаря. Проверь селектор.");
-            }
-
-            arrows.get(arrows.size() - 1).click();
-            Selenide.sleep(500);
-        }
-
-
-        $$("table.calendar__layout td")
-                .find(Condition.text(dayText))
-                .click();
-
-        /*$("span[data-test-id='date'] button span.input__box [placeholder='Дата встречи']")
-                .doubleClick().sendKeys(planningDate);*/
-        $("[data-test-id='name'] input").setValue("Осепчук Никита");
-        $("[data-test-id='phone'] input").setValue("+79110126430");
-        $("[data-test-id='agreement']").click();
-
-        $(Selectors.withText("Забронировать")).click();
-        $("div[data-test-id='notification']")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(text("Встреча успешно забронирована на"));
+        $(".notification__content")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text(" Встреча успешно забронирована на " + planningDate));
 
     }
 
     @Test
-    void shouldRegisterCardDeliveryDay() {
+    void cardWithDeliveryTest1() {
 
-
-        LocalDate target = LocalDate.now().plusDays(15);
-        String dayText = String.format("%02d", target.getDayOfMonth());
-        int monthOffSet = 3;
-
-
-        System.out.println("DEBUG: dayText = '" + dayText + "' (length=" + dayText.length() + ")");
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Ко");
-        $$("div.popup__content div").find(exactText("Кострома")).click();
+        $("[data-test-id='city'] input").setValue("Кострома");
+        String planningDate = generateDate(12, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").click();
+        if (!generateDate(3, "MM").equals(generateDate(7, "MM"))) {
 
-        $("[data-test-id='date'] button").click();
-
-        for (int i = 0; i < monthOffSet; i++) {
-            var arrows = $$("div.calendar__arrow.calendar__arrow_direction_right");
-            if (arrows.isEmpty()) {
-                throw new IllegalStateException(" ");
-            }
-
-            arrows.get(arrows.size() - 1).click();
-            Selenide.sleep(500);
         }
-
-
-        $$("table.calendar__layout td")
-                .find(Condition.text(dayText))
+        $(".calendar__title")
                 .click();
-
-
+        $$(".calendar__layout")
+                .find(Condition.text(generateDate(7, "d")))
+                .click();
         $("[data-test-id='name'] input").setValue("Осепчук Никита");
         $("[data-test-id='phone'] input").setValue("+79110126430");
         $("[data-test-id='agreement']").click();
-
         $(Selectors.withText("Забронировать")).click();
-        $("div[data-test-id='notification']")
+        $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(text("Встреча успешно забронирована на"));
+                .shouldHave(Condition.text("Встреча успешно забронирована на "));
+
 
     }
 }
+
